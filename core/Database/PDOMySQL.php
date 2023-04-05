0509b28cdf652179b255cca24b8d171fc979fb13
miantama guy sebastien <miantamag@gmail.com>
2023-04-05 13:33:50 +0200

    new

--- core/Database/PDOMySQL.php
+++ core/Database/PDOMySQL.php
@@ -4,24 +4,28 @@ namespace Database;
 
 class PDOMySQL
 {
+    public static $currentPdo = null;
 
     public static function getPdo():\PDO
     {
-
         $adresseServeurMySQL = "localhost";
         $nomDeDatabase = "ullie";
         $username = "bloggy";
         $password = "]LhDx@cl6[0tZhxT";
 
-        $pdo = new \PDO("mysql:host=$adresseServeurMySQL;dbname=$nomDeDatabase",
-            $username,
-            $password,
-            [
-                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ
-            ]
-        );
+        if(self::$currentPdo === null){
 
-        return $pdo;
+            self::$currentPdo = new \PDO("mysql:host=$adresseServeurMySQL;dbname=$nomDeDatabase",
+                $username,
+                $password,
+                [
+                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ
+                ]
+            );
+        }
+
+        return self::$currentPdo;
+
     }
 }
